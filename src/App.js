ca6635543c19698615d91aa9fb8912566cdafa30
Archit Singh <architsingh1064@gmail.com>
2022-03-31 10:59:35 +0530

    update small mistake

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
   );
