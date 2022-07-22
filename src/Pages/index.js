881ab7fef2501cd4d582c1fa4fb9b85745a9db06
Archit Singh <architsingh1064@gmail.com>
2022-07-22 13:19:32 +0530

    update directory

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,7 +1,7 @@
 export { HomePage } from "./home/home";
 export { ArchivedPage } from "./archived/archived";
 export { TrashPage } from "./trash/trash";
-export { SignIn } from "./signIn/signIn"
+export { SignIn } from "./SignIn/signIn"
 export { SignUp } from "./signUp/signUp";
 export { PrivateRoute } from "./auth/PrivateRoute";
 export { RestrictedRoute } from "./auth/RestrictedRoute";
